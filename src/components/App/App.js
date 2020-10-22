2ce033138c355e9fc06ecc8d26ecd91255902371
Some0neElse <cyber-def@yandex.ru>
2020-10-22 19:03:09 +0300

    init state of project

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Preloader from "../Preloader/Preloader";
+
+function App() {
+  return (
+    <div className="App">
+      <Preloader />
+    </div>
+  );
+}
+
+export default App;
